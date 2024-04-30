0693e53f8973a2f65506f4dfd9c2490fab3c303f
po2656233 <po2656233@qq.com>
2024-04-30 11:43:22 +0800

    补充提示

--- const/code/code.go
+++ const/code/code.go
@@ -5,6 +5,7 @@ const (
 	SessionUIDNotBind     int32 = 10 // session uid not bind
 	DiscoveryNotFoundNode int32 = 11 // discovery not fond node id
 	NodeRequestError      int32 = 12 // node request error
+
 	RPCNetError           int32 = 20 // rpc net error
 	RPCUnmarshalError     int32 = 21 // rpc base unmarshal error
 	RPCMarshalError       int32 = 22 // rpc base marshal error
@@ -12,30 +13,31 @@ const (
 
 	ActorPathIsNil          int32 = 24 // actor target path is nil
 	ActorFuncNameError      int32 = 25 // actor function name is error
-	ActorConvertPathError   int32 = 26 // convert to path error
-	ActorMarshalError       int32 = 27 // marshal arg error
-	ActorUnmarshalError     int32 = 28 // unmarshal arg error
+	ActorConvertPathError   int32 = 26 // actor convert to path error
+	ActorMarshalError       int32 = 27 // actor marshal arg error
+	ActorUnmarshalError     int32 = 28 // actor unmarshal arg error
 	ActorCallFail           int32 = 29 // actor call fail
-	ActorSourceEqualTarget  int32 = 30 // source equal target
+	ActorSourceEqualTarget  int32 = 30 // actor source equal target
 	ActorPublishRemoteError int32 = 31 // actor publish remote error
 	ActorChildIDNotFound    int32 = 32 // actor child id not found
-
 )
 
-var (
-	Error                    int32 = 1   // error
-	PIDError                 int32 = 100 // pid错误
-	LoginError               int32 = 201 // 登录异常
-	AccountAuthFail          int32 = 202 // 帐号授权失败
-	AccountBindFail          int32 = 203 // 帐号绑定失败
-	AccountTokenValidateFail int32 = 204 // token验证失败
-	AccountNameIsExist       int32 = 205 // 帐号已存在
-	AccountRegisterError     int32 = 206 //
-	AccountGetFail           int32 = 207 //
-	PlayerDenyLogin          int32 = 301 // 玩家禁止登录
-	PlayerDuplicateLogin     int32 = 302 // 玩家重复登录
-	PlayerNameExist          int32 = 303 // 玩家角色名已存在
-	PlayerCreateFail         int32 = 304 // 玩家创建角色失败
-	PlayerNotLogin           int32 = 305 // 玩家未登录
-	PlayerIdError            int32 = 306 // 玩家id错误
-)
+var CodeTxt = map[int32]string{
+	OK:                      "is ok",
+	SessionUIDNotBind:       "session uid not bind",
+	DiscoveryNotFoundNode:   "discovery not fond node id",
+	NodeRequestError:        "node request error",
+	RPCNetError:             "rpc net error",
+	RPCUnmarshalError:       "rpc base unmarshal error",
+	RPCMarshalError:         "rpc base marshal error",
+	RPCRemoteExecuteError:   "rpc remote method executor error",
+	ActorPathIsNil:          "actor target path is nil",
+	ActorFuncNameError:      "actor function name is error",
+	ActorConvertPathError:   "convert to path error",
+	ActorMarshalError:       "actor marshal arg error",
+	ActorUnmarshalError:     "actor unmarshal arg error",
+	ActorCallFail:           "actor call fail",
+	ActorSourceEqualTarget:  "actor source equal target",
+	ActorPublishRemoteError: "actor publish remote error",
+	ActorChildIDNotFound:    "actor child id not found",
+}
