8b84bc23ef25f13dae4a365d6451d8b87c7a96d3
po2656233 <po2656233@qq.com>
2024-10-14 16:08:35 +0800

    支持同节点反馈

--- net/parser/simple/actor_base.go
+++ net/parser/simple/actor_base.go
@@ -43,6 +43,30 @@ func (p *ActorBase) SendMsg(message proto.Message) {
 	}
 }
 
+func (p *ActorBase) SendTo(sid string, message proto.Message) {
+	if onProtoFunc != nil {
+		mid, data, err := onProtoFunc(message)
+		if err != nil {
+			clog.Errorf("[sid = %s,uid = %d] SendMsg fail. [mid = %d, message = %+v]",
+				sid,
+				p.session.Uid,
+				mid,
+				message,
+			)
+			return
+		}
+		rsp := &cproto.PomeloResponse{
+			Sid:  sid,
+			Mid:  mid,
+			Data: data,
+		}
+
+		p.Call(p.session.AgentPath, ResponseFuncName, rsp)
+	} else {
+		clog.Panicf("Did you forget to set SetParseProtoFunc???")
+	}
+}
+
 // Response 响应
 func (p *ActorBase) Response(session *cproto.Session, v interface{}) {
 	Response(p, session, session.Mid, v)
@@ -65,6 +89,42 @@ func (p *ActorBase) Feedback(v interface{}) {
 		Data: data,
 	}
 	p.Call(p.session.AgentPath, ResponseFuncName, rsp)
+}
+
+func SendTo(iActor cfacade.IActor, session *cproto.Session, v interface{}) {
+	if onProtoFunc != nil {
+		mid, data, err := onProtoFunc(v.(proto.Message))
+		if err != nil {
+			clog.Errorf("[sid = %s,uid = %d] SendMsg fail. [mid = %d, message = %+v]",
+				session.Sid,
+				session.Uid,
+				mid,
+				v,
+			)
+			return
+		}
+		rsp := &cproto.PomeloResponse{
+			Sid:  session.Sid,
+			Mid:  mid,
+			Data: data,
+		}
+		iActor.Call(session.AgentPath, ResponseFuncName, rsp)
+		return
+	}
+
+	data, err := iActor.App().Serializer().Marshal(v)
+	if err != nil {
+		clog.Warnf("[Response] Marshal error. v = %+v", v)
+		return
+	}
+
+	rsp := &cproto.PomeloResponse{
+		Sid:  session.Sid,
+		Mid:  session.Mid,
+		Data: data,
+	}
+
+	iActor.Call(session.AgentPath, ResponseFuncName, rsp)
 }
 
 // Response 响应
